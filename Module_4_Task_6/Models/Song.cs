c4827643c0dcaea5d17457b78918b5a413d28e57
Михаил Гринин <Grinin@kh.dcz.gov.ua>
2021-05-05 16:53:56 +0300

    minor fix

--- Module_4_Task_6/Models/Song.cs
+++ Module_4_Task_6/Models/Song.cs
@@ -9,7 +9,7 @@ namespace Module_4_Task_6.Models
     public class Song
     {
         public string Title { get; set; }
-        public IEnumerable<Artist> Artists { get; set; }
+        public string Artist { get; set; }
         public string Genre { get; set; }
     }
 }
